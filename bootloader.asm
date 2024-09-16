82d793a7698740b92b849004ffd2220acece904c
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-09-16 22:12:57 +0900

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -4,33 +4,28 @@ BITS 16
 ORG 0x7C00
 
 start:
-    ; Инициализация сегментных регистров
     xor ax, ax
     mov ds, ax
     mov es, ax
 
-    ; Очистка экрана
     mov ah, 0x00
     mov al, 0x03
     int 0x10
 
-    ; Вывод сообщения
     mov si, msg
     call print_string
 
-    ; Загрузка ядра ОС
-    mov ah, 0x02    ; Функция чтения сектора
-    mov al, 1       ; Количество секторов для чтения
-    mov ch, 0       ; Номер цилиндра
-    mov cl, 2       ; Номер сектора (начиная со второго)
-    mov dh, 0       ; Номер головки
-    mov dl, 0       ; Дисковод (0 = A:)
-    mov bx, 0x1000  ; Адрес загрузки ядра
+    mov ah, 0x02   
+    mov al, 1      
+    mov ch, 0      
+    mov cl, 2       
+    mov dh, 0      
+    mov dl, 0      
+    mov bx, 0x1000 
     mov es, bx
     xor bx, bx
-    int 0x13        ; Вызов BIOS для чтения сектора
+    int 0x13        
 
-    ; Переход к ядру ОС
     jmp 0x1000:0x0000
 
 print_string:
