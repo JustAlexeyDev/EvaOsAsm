b60d46b0bf35b02617237eaf0d88832abd4ecccc
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-12-11 11:40:58 +0900

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -2,28 +2,22 @@ org 0x7C00
 bits 16
 
 start:
-    ; Инициализация сегментных регистров
-    xor ax, ax
+    xor ax, ax         
     mov ds, ax
     mov es, ax
-
-    ; Установка стека
     mov ss, ax
-    mov sp, 0x7C00
+    mov sp, 0x7C00     
 
-    ; Очистка экрана
     call clear_screen
-
-    ; Установка курсора в верхний левый угол
     call set_cursor_top_left
 
-    ; Вывод заголовка
     mov si, header_msg
     call print_string
     call print_newline
 
-    ; Переход к основному коду
-    jmp 0x0000:0x7E00
+    jmp 0x0000:0x8000 
+
+; Остальной код...
 
 clear_screen:
     mov ax, 0x0600
@@ -59,7 +53,7 @@ print_newline:
     int 0x10
     ret
 
-header_msg db 'Eva-OS VioletKernel - version 0.000.425', 0
+header_msg db 'Eva-OS VioletKernel - version 0.000.427', 0
 
 times 510-($-$$) db 0
 dw 0xAA55
