025794505682812a4ae5f0dc8f023cfc37eb5830
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-12-13 16:12:49 +0900

    Delete compirun32.bat

--- compirun32.bat
+++
@@ -1,4 +0,0 @@
-nasm -f bin -o VioletKernel32bit.bin VioletKernel32bit.asm
-nasm -f bin -o bootloader32bit.bin bootloader32bit.asm
-python -c "open('EvaOS32bit.bin', 'wb').write(open('bootloader32bit.bin', 'rb').read() + open('VioletKernel32bit.bin', 'rb').read())"
-qemu-system-x86_64 -fda EvaOS32bit.bin
