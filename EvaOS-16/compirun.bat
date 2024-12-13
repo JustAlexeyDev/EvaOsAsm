8471d41db724126bc6b991c3f9bda377ff0b0558
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-12-13 16:20:59 +0900

    Update compirun.bat

--- EvaOS-16/compirun.bat
+++ EvaOS-16/compirun.bat
@@ -1,4 +1,4 @@
-nasm -f bin -o VioletKernel.bin VioletKernel.asm
+nasm -f bin -o VioletKernel.bin ./core/VioletKernel.asm
 nasm -f bin -o bootloader.bin bootloader.asm
 python -c "open('EvaOS.bin', 'wb').write(open('bootloader.bin', 'rb').read() + open('VioletKernel.bin', 'rb').read())"
 qemu-system-x86_64 -fda EvaOS.bin
