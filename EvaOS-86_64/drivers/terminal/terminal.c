3b4e5490450fd8cb1e48907426750d954f2ea3f9
Sskame1 <tolikstrgonov111@gmail.com>
2025-10-26 18:11:00 +0900

    add drivers/terminal

---
+++ EvaOS-86_64/drivers/terminal/terminal.c
@@ -0,0 +1,93 @@
+#include "drivers/terminal/terminal.h"
+#include "drivers/terminal/vga.h"
+#include "drivers/ports/ports.h"
+
+static size_t terminal_row;
+static size_t terminal_column;
+static uint8_t terminal_color;
+static uint16_t* terminal_buffer;
+
+void terminal_init(void) {
+    terminal_row = 0;
+    terminal_column = 0;
+    terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+    terminal_buffer = (uint16_t*)VGA_BUFFER;
+    
+    for(size_t y = 0; y < VGA_HEIGHT; y++) {
+        for(size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = vga_entry(' ', terminal_color);
+        }
+    }
+}
+
+void terminal_setcolor(uint8_t color) {
+    terminal_color = color;
+}
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+    const size_t index = y * VGA_WIDTH + x;
+    terminal_buffer[index] = vga_entry(c, color);
+}
+
+void terminal_scroll(void) {
+    // Копируем строки на одну вверх
+    for(size_t y = 1; y < VGA_HEIGHT; y++) {
+        for(size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t from_index = y * VGA_WIDTH + x;
+            const size_t to_index = (y - 1) * VGA_WIDTH + x;
+            terminal_buffer[to_index] = terminal_buffer[from_index];
+        }
+    }
+    
+    // Очищаем последнюю строку
+    for(size_t x = 0; x < VGA_WIDTH; x++) {
+        const size_t index = (VGA_HEIGHT - 1) * VGA_WIDTH + x;
+        terminal_buffer[index] = vga_entry(' ', terminal_color);
+    }
+}
+
+void update_cursor(int x, int y) {
+    uint16_t pos = y * VGA_WIDTH + x;
+    
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (uint8_t)(pos & 0xFF));
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
+}
+
+void terminal_putchar(char c) {
+    if(c == '\n') {
+        terminal_column = 0;
+        terminal_row++;
+    } else if(c == '\r') {
+        terminal_column = 0;
+    } else if(c == '\t') {
+        terminal_column = (terminal_column + 4) & ~(4 - 1);
+    } else {
+        terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+        terminal_column++;
+    }
+
+    if(terminal_column >= VGA_WIDTH) {
+        terminal_column = 0;
+        terminal_row++;
+    }
+
+    if(terminal_row >= VGA_HEIGHT) {
+        terminal_scroll();
+        terminal_row = VGA_HEIGHT - 1;
+    }
+    
+    update_cursor(terminal_column, terminal_row);
+}
+
+void terminal_write(const char* data, size_t size) {
+    for(size_t i = 0; i < size; i++) {
+        terminal_putchar(data[i]);
+    }
+}
+
+void terminal_writestring(const char* data) {
+    terminal_write(data, strlen(data));
+}
